373ed1a996797873886a72f4bb7abf912796b19e
MarcoChanwoo <cwhong8087@gmail.com>
2023-11-27 13:25:34 +0900

    updated

--- modules/counter.js
+++ modules/counter.js
@@ -7,3 +7,16 @@ export const decrease = () => ({ type: DECREASE });
 const initialState = {
   number: 0,
 };
+
+function counter(state = initialState, action) {
+  switch (action.type) {
+    case INCREASE:
+      return {
+        number: state.number + 1,
+      };
+      break;
+
+    default:
+      break;
+  }
+}
