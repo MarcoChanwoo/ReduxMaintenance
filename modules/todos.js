22e0ebdaddc9ffcc9f7835c4a9d5e3a5a10c4de7
MarcoChanwoo <cwhong8087@gmail.com>
2023-11-28 11:26:01 +0900

    updated

---
+++ modules/todos.js
@@ -0,0 +1,4 @@
+const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // input값을 변경
+const INSERT = 'todos/INSERT'; // 새로운 todo를 등록
+const TOGGLE = 'todos/TOGGLE'; // todo를 체크/체크 해제
+const REMOVE = 'todos/REMOVE'; // todo 제거
